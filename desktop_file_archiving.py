170cd49bc363b38c4f7d68531f8aa78f8e3ecd01
hamperfect <hamperfect@hotmail.com>
2015-10-29 00:12:47 -0400

    Update and rename organize_files_of_same_extension_to_a_date_based_folder.py to desktop_file_archiving.py

--- desktop_file_archiving.py
+++ desktop_file_archiving.py
@@ -8,3 +8,6 @@ files_on_desktop = os.listdir(desktop_path)
 for file in files_on_desktop:
     if file.endswith('jpg') is True:
 	shutil.move(desktop_path + file, folder_path)
+	
+ # 这段代码的功能是把所有桌面或指定目录下所有的的.jpg文件存到一个以当前日期为名字的文件夹里
+ # 当然，由于没加判断，这里要是已经有那个文件夹的话就会报错，加完判断的代码在desktop_file_archiving_ii.py里
